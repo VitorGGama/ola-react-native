2470f3d9f141c77604eaa7d64f44d9cb2854700b
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-02-07 08:08:56 -0300

    .

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native e Expo! 😎</Text>
+      <Text>React Native e Expo! 😎 </Text>
       <StatusBar style="auto" />
     </View>
   );
