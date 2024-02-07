95ca0698ae4cccc75a972d262aa0aa52e5aacc11
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-02-07 11:00:04 -0300

    estilizando

--- App.js
+++ App.js
@@ -3,25 +3,26 @@ import {
   Button,
   SafeAreaView,
   StatusBar,
+  StyleSheet,
   Text,
   View,
 } from "react-native";
 
 export default function App() {
   const vai = () => {
-    Alert.alert("Titulo da janela", "Fala parça!");
+    Alert.alert("Titulo", "Vai Corinthians!");
   };
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <View>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.cabecalho}>
           <Text>Topo/Cabeçalho</Text>
         </View>
-        <View>
+        <View style={estilos.conteudo}>
           <Text>Conteúdo...</Text>
         </View>
-        <View>
+        <View style={estilos.rodape}>
           <Text>Rodapé...</Text>
           <Button title="Toca aqui" onPress={vai} />
         </View>
@@ -29,3 +30,19 @@ export default function App() {
     </>
   );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "lightblue",
+    flex: 1,
+  },
+  cabecalho: {
+    backgroundColor: "lightgreen",
+  },
+  conteudo: {
+    backgroundColor: "yellow",
+  },
+  rodape: {
+    backgroundColor: "orange",
+  },
+});
